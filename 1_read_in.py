5942d586e6b6e83856a7b14bc45a59aeed9a285c
rho-tullis <rtullis@penguin>
2024-01-29 16:45:11 -0600

    1...py, 2...py -- split off flagging code from read in code

--- 1_read_in.py
+++ 1_read_in.py
@@ -1,5 +1,5 @@
 # 1_read_in.py   Reads in administrative hospital data,
-#                produces some data quality reports.
+#                produces data quality reports.
 #
 # by Rhonda Tullis, 1/05/2024
 # for Orchid Initiative
@@ -10,9 +10,6 @@ from ydata_profiling import ProfileReport, compare
 
 # Location of data (currently CA synthetic data)
 data_url = 'https://github.com/orchid-initiative/synthetic-database-project/files/13679383/csv_HCAIPDD_12-14-2023_1416.csv'
-
-# LARC procedures (both snomed and ICD10)  >>> Testing PX that is present to make sure code works:  80146002
-larcs = ['80146002','65200003','0UH97HZ','0UH98HZ','0UHC7HZ','0UHC8HZ','169553002','0JHD0HZ','0JHD3HZ','0JHF0HZ','0JHF3HZ']
 
 # READ IN DATA
 # Data types
@@ -55,36 +52,26 @@ datatypes = ({'typcare':str,
               'oproc24':str})
 
 # Date formats
-date_formats = {'bthdate':'%m%d%Y','admtdate':'%m%d%Y','dschdate':'%m%d%Y',\
-                'proc_pdt':'%Y%m%d'}
+date_formats = {'admtdate':'%m%d%Y','dschdate':'%m%d%Y','proc_pdt':'%Y%m%d'}
 
 # Date variables
 dates = list(date_formats.keys())
-
-# Procedure variables
-dtype_vars = list(datatypes.keys())
-proc_vars = [x for x in dtype_vars if "proc" in x]
-print('\nList of proc vars')
-print(proc_vars)
 
 # Read in
 df = pd.read_csv(data_url, 
                  dtype=datatypes, 
                  usecols=('typcare','oshpd_id','data_id','pat_id','rln',
-                          'bthdate','sex','ethncty','race1','patcnty','patzip',
+                          'agyradm','sex','ethncty','race1','race_grp','pls_abbr',
+                          'patcnty','patzip',
                           'admtdate','dschdate','dsch_yr','los','srcpo_ns',
-                          'srcroute_ns','admtype_ns','disp','pay_cat','charge',
+                          'srcroute_ns','admtype_ns','disp',
+                          'pay_cat','pay_type','pay_plan','charge','MSDRG',
                           'diag_p','poa_p','proc_p','proc_pdt',
                           'odiag2','odiag3','odiag4','odiag5','odiag6',
                           'odiag7','odiag8','odiag9','odiag10','odiag11',
                           'odiag12','odiag13','odiag14','odiag15','odiag16',
                           'odiag17','odiag18','odiag19','odiag20','odiag21',
                           'odiag22','odiag23','odiag24',
-                          'opoa2','opoa3','opoa4','opoa5','opoa6',
-                          'opoa7','opoa8','opoa9','opoa10','opoa11',
-                          'opoa12','opoa13','opoa14','opoa15','opoa16',
-                          'opoa17','opoa18','opoa19','opoa20','opoa21',
-                          'opoa22','opoa23','opoa24',
                           'oproc2','oproc3','oproc4','oproc5','oproc6',
                           'oproc7','oproc8','oproc9','oproc10','oproc11',
                           'oproc12','oproc13','oproc14','oproc15','oproc16',
@@ -93,22 +80,6 @@ df = pd.read_csv(data_url,
                  parse_dates = dates,
                  date_format=date_formats
                  )
-
-# Set datetime variables to dates (dates without also time didnt work in ProfileReport.
-"""for datevar in dates:
-    print(datevar)
-    print(df[datevar].head())
-    df[f'new_{datevar}'] = pd.to_datetime(df[datevar]).dt.date
-    print(df[f'new_{datevar}'].head())
-# Print 5 rows
-print(f'\nRead in of {data_url}')
-print(df.head())
-"""
-
-# Look for LARC procs
-df['larc'] = 0
-for var in proc_vars:
-    df.loc[df[var].isin(larcs), 'larc'] = 1
 
 report = ProfileReport(df=df, minimal=True)
 report.to_file('readin_report.html')
