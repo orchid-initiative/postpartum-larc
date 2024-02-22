69f600ac82981558a01191e820196c1e84ae8d78
Rhonda Tullis <rhonda.lynnae.tullis@gmail.com>
2024-02-22 15:33:12 -0600

    test_openfile.py -- MAKE HEADER ROW OF SELECT VARS

--- test_openfile.py
+++ test_openfile.py
@@ -1,19 +1,34 @@
 # test pgm to produce readin/subset/transform/aggregate steps 
 # w/small test file
 
-filepath = 
-selection_logic = 
-variables_keep = 
+import csv
+filepath = 'testdata.csv' 
+#selection_logic = 
+variables_keep = ['firstid', 'thirdthing']
 outputfile_prefix = 'labor_and_delivery'
 
-with open(filepath,buffer=, mode='rt') as file:
-    for line, enumerate file:
-        count = 
-        if count==1:
-            headerrow = line (minus single quotes and return char)
-            print("Header row:")
-            print(headerrow)
+# Open the input file, one row at a time,
+# count all records,
+# keep and subset the header row for variables of interest,
+# keep variables of interest,
+# keep selected records
+with open(filepath, buffering=1, mode='rt') as infile:
+    print('Reading input file:  ',filepath)
+    for count, line in enumerate(infile):
+        if count==0:
+            headerrow_all = line.replace('\n','').split(sep=',') #(minus single quotes and return char)
+            print('\nEntire dataset header row:')
+            print(headerrow_all)
+            print('\nVariables of intereste are:')
+            print(variables_keep)
+            headerrow_subset = [x for x in headerrow_all if x in variables_keep]
+            print('\nSubset header row (it should match above):  ')
+            print(headerrow_subset)                    
+            with open(f'{outputfile_prefix}_all.csv', mode='w') as outfile:
+                writer = csv.writer(outfile)
+                writer.writerow(headerrow_subset)
         else:
 
-
+            keep_row = line[header
+            quit()
 
