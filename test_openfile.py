539f50bf81af4c726c951bff511cf9bd1227bd0e
Rhonda Tullis <rhonda.lynnae.tullis@gmail.com>
2024-02-22 12:56:09 -0600

    test_openfile.py -- INITIAL COMMIT FOR TEST PGM ON TINY TEST FILE

---
+++ test_openfile.py
@@ -0,0 +1,19 @@
+# test pgm to produce readin/subset/transform/aggregate steps 
+# w/small test file
+
+filepath = 
+selection_logic = 
+variables_keep = 
+outputfile_prefix = 'labor_and_delivery'
+
+with open(filepath,buffer=, mode='rt') as file:
+    for line, enumerate file:
+        count = 
+        if count==1:
+            headerrow = line (minus single quotes and return char)
+            print("Header row:")
+            print(headerrow)
+        else:
+
+
+
