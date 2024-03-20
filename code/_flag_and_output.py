e9219ef60a602a49c559ba9e0a4bec3ee539d13c
Rhonda Tullis <rhonda.lynnae.tullis@gmail.com>
2024-03-20 18:50:12 -0500

    parameters.py, _flag_and_output.py -- MOVE PARMS TO parameters.py

--- code/_flag_and_output.py
+++ code/_flag_and_output.py
@@ -1,21 +1,16 @@
-# 2_flag_and_output.py  Adds inclusion and exclusion flags,
-#                       and category flags.  Then subsets 
-#                       and outputs data for downstream 
-#                       reporting.
+#######################################################################
+# _flag_and_output.py  Adds inclusion and exclusion flags, and 
+#                      category flags.  Then subsets, aggregates, 
+#                      and outputs data for downstream reporting.
 #
 # by Rhonda Tullis, 1/24/2024
 # for Orchid Initiative
+#######################################################################
 
 import pandas as pd
 import lxml
 import datetime as dt
-from ydata_profiling import ProfileReport, compare
 
-# MSDRGs for Labor & Delivery to select denominator records
-ld_drgs = [768, 788, 807]
-
-# LARC procedures (both snomed and ICD10)  >>> Testing PX that is present to make sure code works:  80146002
-larcs = ['80146002','65200003','0UH97HZ','0UH98HZ','0UHC7HZ','0UHC8HZ','169553002','0JHD0HZ','0JHD3HZ','0JHF0HZ','0JHF3HZ']
 
 # Get lists of codes to use for exclusions and category flags
 specs = "https://docs.google.com/document/d/141xp-JPispApGdvv8b8Ou8yDhxEQ6M6KO9Ats9ZrIHg/edit?usp=sharing"
--- code/parameters.py
+++ code/parameters.py
@@ -1,55 +1,68 @@
 ########################################################################
-#  parameters.py - contains all of the values to be passed to the 
-#                  called programs
+#  parameters.py - User parameter file that contains all of the values 
+#                  to be passed to the called programs
 #
 #  by Rhonda Tullis, 2/28/2024
 ########################################################################
 
-# mainpath = root of your working directory
-mainpath='/home/rtullis/orchid_initiative/postpartum_larc' 
+# Root of your working directory
+mainpath = '/home/rtullis/orchid_initiative/postpartum_larc' 
 
-# inpath = {Path and file name for input file} 
-inpath=f'{mainpath}/raw'
+# Path and file name for input file
+inpath = f'{mainpath}/raw'
 
-# input_files = {a dict w/list of the names of the input files 
-#                and a naming suffix to differentiate output files}
-infile_names=['csv_HCAIPDD_01-31-2024_1538_2018.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2019.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2020.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2021.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2022.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2023.csv',
-              'csv_HCAIPDD_01-31-2024_1538_2024.csv']
+# A dict w/list of the names of the input files 
+# and a naming suffix to differentiate output files.
+infile_names = ['csv_HCAIPDD_01-31-2024_1538_2018.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2019.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2020.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2021.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2022.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2023.csv',
+                'csv_HCAIPDD_01-31-2024_1538_2024.csv']
 
-# output path for interim files
-outpath=f'{mainpath}/created'
+# Output path for interim files
+outpath = f'{mainpath}/created'
 
-# outputfile_prefix = {Used in the name of the output files}
+# Used in the name of the output files
 outputfile_prefix = 'labor_and_delivery'
 
-# selection_variable_name = {Name of variable used to select data}
-filter_variable_name='admtype_ns'
+# Name of variable used to select denominator rows
+#filter_variable_name = 'MSDRG'
+filter_variable_name = 'admtype_ns'
 
-# selection_values = {Desired values for selection_variable_name}
-filter_values=[4, 5, 6]
+# Values for filter_variable_name for selecting denominator rows
+#filter_values = [768, 788, 807] #Labor and delivery DRGs
+filter_values = [4, 5, 6]
 
-# variables_keep = {list of variables to keep in output}
-variables_keep=['oshpd_id', 'data_id', 'pat_id', 'agyradm', 'sex', 
-                'ethncty', 'race1', 'race_grp', 'pls_abbr', 'patcnty',
-                'patzip', 'admtdate', 'dschdate', 'dsch_yr', 'los', 
-                'srcpo_ns', 'srcroute_ns', 'admtype_ns', 'disp', 
-                'pay_cat', 'pay_type', 'pay_plan', 'charge', 'MSDRG', 
-                'diag_p', 'poa_p', 'proc_p', 
-                'odiag2', 'odiag3', 'odiag4', 'odiag5', 'odiag6', 
-                'odiag7', 'odiag8', 'odiag9', 'odiag10', 'odiag11',
-                'odiag12', 'odiag13', 'odiag14', 'odiag15', 'odiag16',
-                'odiag17', 'odiag18', 'odiag19', 'odiag20', 'odiag21',
-                'odiag22', 'odiag23', 'odiag24', 
-                'oproc2', 'oproc3', 'oproc4', 'oproc5', 'oproc6',
-                'oproc7', 'oproc8', 'oproc9', 'oproc10', 'oproc11',
-                'oproc12', 'oproc13', 'oproc14', 'oproc15', 'oproc16',
-                'oproc17', 'oproc18', 'oproc19', 'oproc20', 'oproc21',
-                'oproc22', 'oproc23', 'oproc24']
+# LARC procedures (both snomed and ICD10)  >>> Testing PX that is present to make sure code works:  80146002
+larcs = ['80146002','65200003','0UH97HZ','0UH98HZ','0UHC7HZ','0UHC8HZ','169553002','0JHD0HZ','0JHD3HZ','0JHF0HZ','0JHF3HZ']
+
+# Date formats for date variables that are read in and kept
+date_formats = {'admtdate':'%m%d%Y','dschdate':'%m%d%Y'}
+
+# Variable list for frequency report
+value_count_variables = ['MSDRG','oshpd_id','dsch_yr',
+                         'ethncty','race1','pls_abbr',
+                         'pay_cat','pay_type','pay_plan']
+
+# List of variables to keep in record-level output}
+variables_keep = ['oshpd_id', 'data_id', 'pat_id', 'agyradm', 'sex', 
+                  'ethncty', 'race1', 'race_grp', 'pls_abbr', 'patcnty',
+                  'patzip', 'admtdate', 'dschdate', 'dsch_yr', 'los', 
+                  'srcpo_ns', 'srcroute_ns', 'admtype_ns', 'disp', 
+                  'pay_cat', 'pay_type', 'pay_plan', 'charge', 'MSDRG', 
+                  'diag_p', 'poa_p', 'proc_p', 
+                  'odiag2', 'odiag3', 'odiag4', 'odiag5', 'odiag6', 
+                  'odiag7', 'odiag8', 'odiag9', 'odiag10', 'odiag11',
+                  'odiag12', 'odiag13', 'odiag14', 'odiag15', 'odiag16',
+                  'odiag17', 'odiag18', 'odiag19', 'odiag20', 'odiag21',
+                  'odiag22', 'odiag23', 'odiag24', 
+                  'oproc2', 'oproc3', 'oproc4', 'oproc5', 'oproc6',
+                  'oproc7', 'oproc8', 'oproc9', 'oproc10', 'oproc11',
+                  'oproc12', 'oproc13', 'oproc14', 'oproc15', 'oproc16',
+                  'oproc17', 'oproc18', 'oproc19', 'oproc20', 'oproc21',
+                  'oproc22', 'oproc23', 'oproc24']
 
 # Data types for kept variables that need a string type indicated
 datatypes = ({'oshpd_id':str, 
@@ -111,10 +124,3 @@ datatypes = ({'oshpd_id':str,
               'oproc23':str,
               'oproc24':str})
 
-# Date formats for date variables that are read in and kept
-date_formats = {'admtdate':'%m%d%Y','dschdate':'%m%d%Y'}
-
-# Variable list for frequency report
-value_count_variables = ['MSDRG','oshpd_id','dsch_yr',
-                         'ethncty','race1','pls_abbr',
-                         'pay_cat','pay_type','pay_plan']
