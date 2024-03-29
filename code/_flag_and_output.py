a93cf96dc8b79dd777881cd3f402703e019a3918
Rhonda Tullis <rhonda.lynnae.tullis@gmail.com>
2024-03-28 19:47:56 -0500

    _flag_and_output.py -- ADD CONDITIONS FLAGS, OUTPUT SAMPLE

--- code/_flag_and_output.py
+++ code/_flag_and_output.py
@@ -13,48 +13,134 @@ import parameters as parm
 import _get_mappings as mappings
 
 # FLAG PROCEDURES, DEMOGRAPHICS, AND CONDITIONS
-def flags(df):
+def flags(df, file_suffix, outpath):
 
-    # LARC procedures
+    # ******** LARC **********
+    # Get list of PXs for LARCs
+    larc_procedures = mappings.get_codelists(source=parm.code_sets,
+                                             sheet_name='LARC') 
+    # Set flag to zero for all records
     df['larc'] = 0
+    # Set records where a PX is in the LARC list
     for var in parm.px_vars:
-        df.loc[df[var].isin(mappings.larc_procedures), 'larc'] = 1
+        df.loc[df[var].isin(larc_procedures), 'larc'] = 1
+        
 
-    # Medicaid payer (if pay_cat=Medi-Cal)
+    # ******** MEDICAID ******** (if pay_cat=Medi-Cal)
     # else zero.  Missing pay_cat values will return missing values.
-    df['medicaid'] = np.where(df['pay_cat']==3, 1, 0) 
+    df['medicaid'] = np.where(df['pay_cat']=='03', 1, 0) 
 
-    # Latinx
-    df.loc[df['ethncty']=='E1', 'latinx'] = 1
 
-    # Non-white
-    df.loc[df['race1'].isin('R1','R2','R3','R4','R9'), 'nonwhite'] = 1
+    # ******** PREFERRED_LANGUAGE_NOT_ENGLISH ********
+    df['preferred_language_not_english'] = np.where(df['pls_abbr']=='ENG', 0, 1)
 
-    # Preferred language not English
-    df.loc[df['pls_abbr']=='ENG', 'non_english_speaking'] = 1
 
-    # Prior pregnancies
+    # ******** PRIOR PREGNANCIES ********
+    # Get list of DXs for prior pregnancies
+    prior_pregnancy_dxs = mappings.get_codelists(source=parm.code_sets,
+                                     sheet_name='prior pregnancy')
+    # Set flag to zero for all records
     df['known_prior_pregnancy'] = 0
+    # Set records where a DX is in the prior pregnancy list
     for var in parm.dx_vars:
-        df.loc[df[var].isin(mappings.prior_pregnancy_dxs), 'known_prior_pregnancy'] = 1
+        df.loc[df[var].isin(prior_pregnancy_dxs), \
+                'known_prior_pregnancy'] = 1
 
-    # Mental illness DX3 (first 3 characters of DX)
+
+    # ******** MENTAL ILLNESS ********
+    # Get list of DX3s for mental illness
+    mental_illness_dx3s = mappings.get_codelists(source=parm.code_sets,
+                                     sheet_name='mental illness')
+    # Set flag to zero for all records
     df['mental_illness'] = 0
+    # Set records where first 3 chars of a DX is in mental illness list
     for var in parm.dx_vars:
-        df.loc[df[var][0:3].isin(mappings.mental_illness_dx3s), 'mental_illness'] = 1
+        df.loc[df[var].str[:3].isin(mental_illness_dx3s), \
+                'mental_illness'] = 1
 
-    # Intellectual disability DX3
+
+    # ******** INTELLECTUAL DISABILITY ********
+    # Get list of DX3s for intellectual disability
+    intellectual_disability_dx3s = mappings.get_codelists(
+            source=parm.code_sets,
+            sheet_name='intellectual disability')
+    # Set flag to zero for all records
     df['intellectual_disability'] = 0
+    # Set records where first 3 chars of a DX in intellctl disablty list
     for var in parm.dx_vars:
-        df.loc[df[var][0:3].isin(mappings.intellectual_disability_dx3s),\
+        df.loc[df[var].str[:3].isin(intellectual_disability_dx3s),\
                 'intellectual_disability'] = 1
 
-    check_these = ['larc','medicaid','latinx','nonwhite',\
-                   'non_english_speaking','known_prior_pregnancy',\
-                   'mental_illness','intellectual_disability']
-    for vars in check_these:
-        print(f'\nCheck -- {var}')
+
+    # ******** HEMORRAHAGE ********
+    # Get list of DXs for hemorrhage 
+    hemorrhage_dxs = mappings.get_codelists(source=parm.code_sets,
+                                            sheet_name='hemorrhage')
+    # Set flag to zero for all records
+    df['hemorrhage'] = 0
+    # Set records where a DX is in the hemorrhage list
+    for var in parm.dx_vars:
+        df.loc[df[var].isin(hemorrhage_dxs), 'hemorrhage'] = 1
+
+
+    # ******** INTRAAMNIOTIC INFECTION ********
+    # Get list of DXs for intraamniotic infection
+    intraamniotic_infection_dxs = mappings.get_codelists(\
+            source=parm.code_sets,
+            sheet_name='intraamniotic infection')
+    # Set flag to zero for all records
+    df['intraamniotic_infection'] = 0
+    # Set records where a DX is in the intraamniotic infection list
+    for var in parm.dx_vars:
+        df.loc[df[var].isin(intraamniotic_infection_dxs), \
+                'intraamniotic_infection'] = 1
+
+
+    # ******** CHORIOAMNIONITIS ********
+    # Get list of DXs for chorioamnionitis
+    chorioamnionitis_dxs = mappings.get_codelists(source=parm.code_sets,
+                                     sheet_name='chorioamnionitis')
+    # Set flag to zero for all records
+    df['chorioamnionitis'] = 0
+    # Set records where a DX is in the chorioamnionitis list
+    for var in parm.dx_vars:
+        df.loc[df[var].isin(chorioamnionitis_dxs), \
+                'chorioamnionitis'] = 1
+
+
+    # ******** ENDOMETRITIS ********
+    # Get list of DXs for endometritis
+    endometritis_dxs = mappings.get_codelists(source=parm.code_sets,
+                                     sheet_name='endometritis')
+    # Set flag to zero for all records
+    df['endometritis'] = 0
+    # Set records where a DX is in the endometritis list
+    for var in parm.dx_vars:
+        df.loc[df[var].isin(endometritis_dxs), \
+                'endometritis'] = 1
+
+
+    # Report on flags
+    check_these = ['larc','pay_cat','medicaid',
+                   'pls_abbr','preferred_language_not_english',
+                   'known_prior_pregnancy','mental_illness',
+                   'intellectual_disability','hemorrhage',
+                   'intraamniotic_infection','chorioamnionitis',
+                   'endometritis']
+
+    for var in check_these:
+        print(f'\nResults for -- {var}')
         print(df[var].value_counts(dropna=False))
+
+
+    # Output to csv
+    record_info_vars = ['oshpd_id','agyradm','sex','ethncty','race1',
+                        'dsch_yr','disp','MSDRG']
+    
+    vars_in_output = record_info_vars + check_these
+
+    df.to_csv(f'{outpath}/sample_{file_suffix}.csv',index=False,
+              columns=vars_in_output)
 
     return df
 
