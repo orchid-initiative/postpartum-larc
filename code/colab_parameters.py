43f50f3ef75aad84e8cc24bcd4ba548602399300
Rose DiLoreto <diloreto.rose@gmail.com>
2026-08-17 15:05:04 -0400

    Create colab_parameters.py
    Created file! Should allow running call.py in a Google Colab notebook

---
+++ code/colab_parameters.py
@@ -0,0 +1,15 @@
+from parameters import *
+
+mainpath = "/content/drive/MyDrive/Immediate Post-Partum LARC Utilization/"
+inpath = "/content/drive/MyDrive/Immediate Post-Partum LARC Utilization/sample -- RECORD LEVEL -- output"
+outpath = "/content/drive/MyDrive/Immediate Post-Partum LARC Utilization/sample aggregations"
+
+infile_names = [
+    "sample_2018.csv",
+    "sample_2019.csv",
+    "sample_2020.csv",
+    "sample_2021.csv",
+    "sample_2022.csv",
+    "sample_2023.csv",
+    "sample_2024.csv",
+]
